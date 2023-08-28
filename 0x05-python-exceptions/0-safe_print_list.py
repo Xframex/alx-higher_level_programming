367a587de2f1f52188920d071f1d94bc74497268
Your Name <you@example.com>
2023-08-28 19:28:00 +0000

    safe print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-
-    try:
-        count = 0
-        for element in my_list:
-            if count < x:
-                print(element, end=' ')
-                count += 1
-        print()  # Print a new line after printing the elements
-    except:
-        pass
-    return count
+    ret = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return ret
