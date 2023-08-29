ed8ec715d449e223a5bf4614e57642f1f922486a
Xframex <loukili.issmael@gmail.com>
2023-08-29 16:49:32 -0700

    size of square class

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""Define a class."""
+
+
 class Square:
+    """form of  a square."""
+
     def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
