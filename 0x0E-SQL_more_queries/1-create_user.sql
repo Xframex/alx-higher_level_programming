9109e0d0904403317f1f640b874b1b6e45df8389
Xframex <loukili.issmael@gmail.com>
2023-10-18 14:39:02 -0700

    how become root

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Lists all privileges of a user to become root or superuser
+
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
