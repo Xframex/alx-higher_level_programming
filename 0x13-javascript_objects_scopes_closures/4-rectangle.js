b132fa2f983c1bf3c102dba02acfbc54718e1351
Xframex <loukili.issmael@gmail.com>
2023-11-14 12:10:13 -0800

    rectangle instance

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*
-class Rectangle a rectangle
+class Rectangle that defines a rectangle
 */
 module.exports = class Rectangle {
   constructor (w, h) {
@@ -9,12 +9,23 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
-  // Prints the rectangle  X
+  // Prints the rectangle using the character X
 
   print () {
     let vtcal;
     for (vtcal = 0; vtcal < this.height; vtcal++) {
       console.log('X'.repeat(this.width));
     }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width = (this.width * 2);
+    this.height = (this.height * 2);
   }
 };
