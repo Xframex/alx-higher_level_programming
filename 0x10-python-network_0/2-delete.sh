a6d289eaca92f0c61c15e7c1449d40e885ca012d
Xframex <loukili.issmael@gmail.com>
2023-11-30 13:47:55 -0800

    fix task 2

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Bash script that sends a DELETE request to the URL passed and displays the body response
+#Bash that sends a DELETE request URL passed and displays the body response
 curl -sX DELETE "$1"
