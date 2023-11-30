a7c4129a17ed0c927075b027883618baa8402ba5
Xframex <loukili.issmael@gmail.com>
2023-11-30 13:44:25 -0800

    fix task 1

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#Bash script that takes displays the size of the body response of an url
+# script that takes displays the size of the body response of an url
 curl -sI "$1" | grep 'Content-Length:' | cut -d " " -f 2
