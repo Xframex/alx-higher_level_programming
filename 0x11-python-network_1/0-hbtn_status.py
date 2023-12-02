97ff7ba9fe40d3ce0815de5d7d7288a459582f6a
Xframex <loukili.issmael@gmail.com>
2023-12-01 16:23:16 -0800

    fetches

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""urllib reponse"""
+
+if __name__ == "__main__":
+    import urllib.request as request
+    holb_url = "https://intranet.hbtn.io/status"
+    with request.urlopen(holb_url) as response:
+        content = response.read()
+        utf8_content = content.decode('utf-8')
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(utf8_content))
