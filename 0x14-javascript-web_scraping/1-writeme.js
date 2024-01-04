a9a077298b01b65507cfcc3db800ee1963688e38
Xframex <loukili.issmael@gmail.com>
2024-01-03 16:32:32 -0800

    fix task1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-/*
-Writes content of a file.
-*/
 const fs = require('fs');
-// Rewriting the arv 
-const argv = process.argv.slice(2);
-fs.writeFile(argv[0], argv[1], 'utf8', function (err) {
-  if (err) {
-    return console.log(err);
-  }
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
